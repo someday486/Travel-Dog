d67de5b6efc6188eebf14c15c8f1a67b7b0f7ba8
someday486 <dongdonglee486@gmail.com>
2024-05-27 17:37:59 +0900

    main

--- trips/views.py
+++ trips/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    alluser = User.objects.all();
+    print(alluser)
+    content = {
+        'phonebook':alluser
+    }
+    return render(request, 'trips/index.html', content);
