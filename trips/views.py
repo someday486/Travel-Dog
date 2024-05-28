d53706b3cca0c56104cc556b14341e77813d2b16
someday486 <dongdonglee486@gmail.com>
2024-05-28 10:57:06 +0900

    trips

--- trips/views.py
+++ trips/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Region, Trip
 
-# Create your views here.
+@login_required
 def index(request):
-    alluser = User.objects.all();
-    print(alluser)
-    content = {
-        'alluser':alluser
-    }
-    return render(request, 'trips/index.html', content);
+    if request.method == 'POST':
+        region_id = request.POST.get('selected_region')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        region = Region.objects.get(id=region_id)
+        Trip.objects.create(user=request.user, region=region, start_date=start_date, end_date=end_date)
+        return redirect('next_page')  # 다음 페이지로 이동
+
+    regions = Region.objects.all()
+    return render(request, 'trips/index.html', {'regions': regions})
+
+@login_required
+def next_page(request):
+    return render(request, 'destinations/location.html')  # 다음 페이지 템플릿으로 이동
