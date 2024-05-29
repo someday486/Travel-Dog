5d2721f93e5fa36f9f84780ab0202444f9bc8116
bejungsang <gktfrance@naver.com>
2024-05-29 17:42:07 +0900

    ㅇㅇ

--- expense/admin.py
+++ expense/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Expense
+from .models import ExpenseDetail
 
 # Register your models here.
-admin.site.register(Expense)
+admin.site.register(ExpenseDetail)
