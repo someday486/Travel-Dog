20394074aa3819da63a9bf4a594ca9b90740fe9e
rmadudtjr1 <rmadudtjr1@gmail.com>
2024-06-04 09:21:54 +0900

    index2 수정

--- expense/urls.py
+++ expense/urls.py
@@ -7,4 +7,5 @@ app_name = 'EP'
 urlpatterns = [
     path("", views.index),
     path('index2/<int:trip_id>/', views.index2, name="E"),
+    path('fileremove/<trip_detail_id>', views.file_remove)
 ]
