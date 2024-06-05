ef7ae4a4112bb2bf6e8951004901df89968cac95
응애 <dongdonglee486@gmail.com>
2024-06-05 16:13:14 +0900

    .

--- myweb/urls.py
+++ myweb/urls.py
@@ -23,7 +23,7 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index),
+    path("", views.home),
     path("home/", views.home),
     path('expense/', include('expense.urls')),
     path('trips/', include('trips.urls')),
